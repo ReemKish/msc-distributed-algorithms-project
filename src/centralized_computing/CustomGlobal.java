238cd4034c5712b0751dd79458eba4609befc647
Re'em Kishinevsky <reem.kishinevsky@gmail.com>
2023-02-22 23:32:23 +0200

    Removed redundant code from CustomGlobal now that the whole graph building logic has been shifted to the custom Distribution and Connectivity models.

--- src/centralized_computing/CustomGlobal.java
+++ src/centralized_computing/CustomGlobal.java
@@ -1,23 +1,12 @@
 package projects.centralized_computing;
 
-import java.util.Vector;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
-
-import projects.centralized_computing.nodes.nodeImplementations.GraphNode;
-
-import sinalgo.configuration.Configuration;
 import sinalgo.runtime.AbstractCustomGlobal;
-import sinalgo.runtime.Runtime;
 import sinalgo.tools.Tools;
 
 public class CustomGlobal extends AbstractCustomGlobal {
 
-  public static java.util.Random rand = sinalgo.tools.Tools.getRandomNumberGenerator(); 
-  public static boolean showWeights = true;
+  public static boolean showWeights = false;
   public static int round = 0;
-  public static int randomEdgeWeight;
 
   public boolean hasTerminated() {
     return (round > 10);
@@ -37,24 +26,6 @@ public class CustomGlobal extends AbstractCustomGlobal {
     return 1 + logStar(Math.log(x) / Math.log(2));
   }
 
-  public static Set<Integer> sampleIntegersInRange(int start, int end, int count) {
-        if (end < start || count > (end - start + 1)) {
-            throw new IllegalArgumentException("Invalid range or count specified.");
-        }
-        java.util.Random rand = sinalgo.tools.Tools.getRandomNumberGenerator(); 
-        Set<Integer> sampled = new HashSet<>();
-        while (sampled.size() < count) {
-            int candidate = rand.nextInt(end - start + 1) + start;
-            sampled.add(candidate);
-        }
-        return sampled;
-    }
-
-  @AbstractCustomGlobal.CustomButton(buttonText = "Build Graph", toolTipText = "Builds the graph")
-  public void buildGraphButton() {
-    int numNodes = Integer.parseInt(Tools.showQueryDialog("Number of nodes:"));
-    buildGraph(numNodes);
-  }
 
   @AbstractCustomGlobal.CustomButton(buttonText = "Toggle Weights", toolTipText = "Toggle visibility of the edges' weights.")
   public void toggleWeightsButton() {
@@ -62,10 +33,5 @@ public class CustomGlobal extends AbstractCustomGlobal {
     Tools.repaintGUI();
   }
 
-  // a vector of all non-leaf nodes
-  Vector<GraphNode> nodes = new Vector<GraphNode>();
-
-  public void buildGraph(int numNodes) {
-  }
 }
 
