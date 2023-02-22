6376a38324ad1fcfd9c6e31128d33095ea382f82
Re'em Kishinevsky <reem.kishinevsky@gmail.com>
2023-02-22 20:26:12 +0200

    Adjust CustomGlobal to fit the new distribution model logic.

--- src/centralized_computing/CustomGlobal.java
+++ src/centralized_computing/CustomGlobal.java
@@ -11,7 +11,6 @@ import sinalgo.configuration.Configuration;
 import sinalgo.runtime.AbstractCustomGlobal;
 import sinalgo.runtime.Runtime;
 import sinalgo.tools.Tools;
-import sinalgo.tools.statistics.UniformDistribution;
 
 public class CustomGlobal extends AbstractCustomGlobal {
 
@@ -67,38 +66,6 @@ public class CustomGlobal extends AbstractCustomGlobal {
   Vector<GraphNode> nodes = new Vector<GraphNode>();
 
   public void buildGraph(int numNodes) {
-    if (numNodes <= 0) {
-      Tools.showMessageDialog("The number of nodes needs to be at least 1.\nCreation of graph aborted.");
-      return;
-    }
-    Runtime.clearAllNodes();
-    nodes.clear();
-    GraphNode.nodeIdCounter = 0;
-    double angle, distance = 0;
-    double posX, posY = 0;
-    double radius = (double) Math.min(Configuration.dimX, Configuration.dimY) / 2.1;
-    UniformDistribution uniform = new UniformDistribution(0, 1);
-    GraphNode gn;
-    // Generate graph vertices.
-    for (int i = 0; i < numNodes; i++) {
-      angle = uniform.nextSample() * 2 * Math.PI;
-      distance = Math.sqrt(uniform.nextSample()) * radius;
-      posX = distance * Math.cos(angle) + Configuration.dimX / 2;
-      posY = distance * Math.sin(angle) + Configuration.dimY / 2;
-      gn = new GraphNode();
-      gn.finishInitializationWithDefaultModels(true);
-      nodes.add(gn);
-      gn.setPosition(posX, posY, 0);
-    }
-    // Generate graph edges.
-    for (int i = 0; i < numNodes; i++) {
-      Set<Integer> neighbors = sampleIntegersInRange(0, numNodes-1, 3);
-      for (int neigh : neighbors) {
-        randomEdgeWeight = rand.nextInt(1000000000 - 1) + 1;
-        nodes.get(i).addConnectionTo(nodes.get(neigh));
-      }
-    }
-    Tools.repaintGUI();
   }
 }
 
