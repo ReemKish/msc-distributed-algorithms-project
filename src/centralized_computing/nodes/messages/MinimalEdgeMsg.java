92fdf9368977f92866f06acc7c7f8c81757d2620
Re'em Kishinevsky <reem.kishinevsky@gmail.com>
2023-02-25 22:42:24 +0200

    MinimalEdgeMsg saves the whole edge instead of its ID.

--- src/centralized_computing/nodes/messages/MinimalEdgeMsg.java
+++ src/centralized_computing/nodes/messages/MinimalEdgeMsg.java
@@ -1,11 +1,12 @@
 package projects.centralized_computing.nodes.messages;
 
+import projects.centralized_computing.nodes.edges.WeightedEdge;
 import sinalgo.nodes.messages.Message;
 
 public class MinimalEdgeMsg extends Message {
-  public int weight;
+  public WeightedEdge mwoe;
 
-	public MinimalEdgeMsg(int weight) { this.weight = weight; }
+	public MinimalEdgeMsg(WeightedEdge mwoe) { this.mwoe = mwoe; }
 
 	@Override
 	public Message clone() { return this; }
