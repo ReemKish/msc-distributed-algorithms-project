4e87d48253796bd9d16f189a204070f57da9f591
Re'em Kishinevsky <reem.kishinevsky@gmail.com>
2023-02-25 19:39:24 +0200

    Removed hack to ensure bidirectional edges have the same weight - replaced by static mapping; Modified getColor() and draw() methods of WeightedEdge such that edges color and transparency correspond to the edge type (e.g. gray inter-fragment edges, colored intra-fragment edges, bold tree edges).

--- src/centralized_computing/nodes/edges/WeightedEdge.java
+++ src/centralized_computing/nodes/edges/WeightedEdge.java
@@ -1,11 +1,15 @@
 package projects.centralized_computing.nodes.edges;
-
 import projects.centralized_computing.CustomGlobal;
 import projects.centralized_computing.nodes.nodeImplementations.GraphNode;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+import java.util.Objects;
 
 import sinalgo.configuration.Configuration;
 import sinalgo.configuration.CorruptConfigurationEntryException;
@@ -19,29 +23,28 @@ import sinalgo.nodes.edges.BidirectionalEdge;
 import sinalgo.tools.Tools;
 
 public class WeightedEdge extends BidirectionalEdge {
-  static final int BILLION = 1000000000;
+  static final int BILLION = 100;
   private static Random rand = sinalgo.tools.Tools.getRandomNumberGenerator(); 
-  private static int currWeight = rand.nextInt(BILLION) + 1;
+  private static Map<Set<Integer>, Integer> edgeWeights = new HashMap();
 
   private boolean show;
   public int weight;
   public GraphNode startNode;
   public GraphNode endNode;
 
-	public WeightedEdge() {
-    // Very hacky way to ensure bidirectional edges have same weight on each direction.
-    if (currWeight > BILLION) {
-      weight = currWeight - BILLION;
-      currWeight = rand.nextInt(BILLION) + 1;
-    } else {
-      weight = currWeight;
-      currWeight += BILLION;
-    }
-	}
 
+  @Override
 	public void initializeEdge() {
+    super.initializeEdge();
     startNode = (GraphNode) super.startNode;
     endNode = (GraphNode) super.endNode;
+    Set<Integer> endpoints = Set.of(startNode.ID, endNode.ID);
+    if (edgeWeights.containsKey(endpoints))
+      weight = edgeWeights.get(endpoints);
+    else {
+      weight = rand.nextInt(BILLION) + 1;
+      edgeWeights.put(endpoints, weight);
+    }
 	}
 
 	public void draw(Graphics g, PositionTransformation pt) {
@@ -54,16 +57,21 @@ public class WeightedEdge extends BidirectionalEdge {
 
     // Show weight label on the edge near the origin vertex.
     if (CustomGlobal.showWeights) {
-      int weightLabelX = (int)(0.95*fromX + 0.05*toX);
-      int weightLableY = (int)(0.95*fromY + 0.05*toY);
+		  g.setColor(Color.BLACK);
+      int weightLabelX = (int)(0.9*fromX + 0.1*toX);
+      int weightLableY = (int)(0.9*fromY + 0.1*toY);
       g.drawString(Integer.toString(weight), weightLabelX, weightLableY);
     }
 
     // System.out.println(startNode + " " + endNode);
-    if(startNode.isParentOf(endNode))
+		g.setColor(getColor());
+    if(endNode.isParentOf(startNode) && startNode.isParentOf(endNode)) {
+		  GraphPanel.drawBoldLine(g, fromX, fromY, toX, toY, 3);
+		  Arrow.drawArrowHead(fromX, fromY, toX, toY, g, pt, getColor());
+    }
+    else if(endNode.isParentOf(startNode))
 		  Arrow.drawArrow(fromX, fromY, toX, toY, g, pt, getColor());
-    else if (!endNode.isParentOf(startNode)) {
-      g.setColor(Color.lightGray);
+    else if(!startNode.isParentOf(endNode)) {
       g.drawLine(fromX, fromY, toX, toY);
     }
 		  // GraphPanel.drawBoldLine(g, fromX, fromY, toX, toY, 3);
@@ -86,11 +94,29 @@ public class WeightedEdge extends BidirectionalEdge {
 		// }
 	}
 
+
+
+  public static Color setColorAlpha(Color color, float alpha) {
+    int red = color.getRed();
+    int green = color.getGreen();
+    int blue = color.getBlue();
+    return new Color(red, green, blue, (int)(alpha * 255));
+  }
+
   public String toString() {
     return "Weight: " + weight;
   }
 
   public Color getColor() {
-    return weight < 1000000000 / 5 ? Color.BLACK : Color.LIGHT_GRAY;
+    Color color;
+    if(startNode.fragID == endNode.fragID) {  // Intrafragment edge.
+      color = startNode.getColor();
+      color = setColorAlpha(color, startNode.isRelatedTo(endNode) ? 1.0f : 1.0f);
+    } else {  // Interfragment edge.
+      color = setColorAlpha(Color.GRAY, 0.2f);
+    }
+    return color;
+    // return Color.RED;
+    // return weight < 1000000000 / 5 ? Color.BLACK : Color.LIGHT_GRAY;
   }
 }
